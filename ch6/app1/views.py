17e8997721d745fc351d81ad31c47dbf9b57a865
Saksham Lodha <legitsaksham@gmail.com>
2026-01-01 21:03:43 +0530

    learned how to work a little with views and url

--- ch6/app1/views.py
+++ ch6/app1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def myfunction(request):
+    # logicis written here
+    return HttpResponse("Hellow django")
+
+
+def myfunction1(request):
+    # logicis written here
+    return HttpResponse("Hellow inflict")
