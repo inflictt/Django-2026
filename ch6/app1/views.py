957f498238104ee4082477499850922dba1f9bee
Saksham Lodha <legitsaksham@gmail.com>
2026-01-01 21:11:37 +0530

    learned how to work a little with views and url

--- ch6/app1/views.py
+++ ch6/app1/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def home(request):
+    # logicis written here
+    return HttpResponse("Hellow django to you homePage.!!1")
+
+
 def myfunction(request):
     # logicis written here
     return HttpResponse("Hellow django")
--- ch6/ch6/urls.py
+++ ch6/ch6/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 from app1 import views
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
     path('myfun/', views.myfunction, name='myfunction'),
     path('myfun1/', views.myfunction1, name='myfunction1'),
 ]
